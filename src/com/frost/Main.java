89cedd09c9de024748b65a25320627ff771f53af
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-03-19 22:33:13 +0300

    Initial commit

---
+++ src/com/frost/Main.java
@@ -0,0 +1,52 @@
+package com.frost;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Перепись населения
+*/
+
+public class Main {
+    public static Map<String, String> createMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Смирнов", "Сильвестр");
+        map.put("Иванов", "Дмитрий");
+        map.put("Кузнецов", "Арнольд");
+        map.put("Соколов", "Роман");
+        map.put("Попов", "Максим");
+        map.put("Лебедев", "Ярослав");
+        map.put("Новиков", "Александр");
+        map.put("Козлов", "Михаил");
+        map.put("Морозов", "Козлов");
+        map.put("Петров", "Алексей");
+        return map;
+    }
+
+    public static int getCountTheSameFirstName(Map<String, String> map, String name) {
+        int count = 0;
+        for (String nameTmp : map.values()) {
+            System.out.println(nameTmp);
+            System.out.println(map.values());
+            if (nameTmp.equals(name)) {
+                count++;
+            }
+            System.out.println(count);
+        }
+        return count;
+    }
+
+    public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
+        int count = 0;
+        for (String lastNameTmp : map.keySet()) {
+            if (lastNameTmp.equals(lastName)) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
